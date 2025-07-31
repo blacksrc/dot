165eda4574cc2c0bbb1b3e66d6f00cac99445dc7
Siamak <siamak.aghaeipour@gmail.com>
2025-07-31 11:25:47 +0200

    chore: update intro

--- intro.sh
+++ intro.sh
@@ -12,20 +12,17 @@ cat << "EOF"
 ██║  ██║██║   ██║   ██║   
 ██████╔╝╚██████╔╝   ██║   
 ╚═════╝  ╚═════╝    ╚═╝   
-                        
-🚀 Version: 0.0.1
-📄 License: MIT
-
-📝 Description:
-This script automates the setup of a development environment by installing essential tools and applications,
-including Oh My Zsh, NVM, VSCode, and more.
+Version: 0.0.1
+License: MIT
+Description: This script automates the setup of a development environment
+by installing essential tools and applications, including Oh My Zsh, NVM, VSCode, and more.
 
 =============================================================================
 🔔 Important Notes:
 - This is a personal, non-optimized setup script tailored to my preferences.
 Feel free to customize it to fit your own needs!
 - This script installs various development tools and modifies your system.
-- Please backup your important data before proceeding.
+- Please backup your important data before proceeding (probably nothing to backup yet).
 - Recommended for fresh Ubuntu or Debian installations.
 - Must be run with root privileges (e.g., via sudo).
 - Review and understand the script before running it.
